c022989a661d16596b580eefb199368be7af42d7
Fesposito9197 <kekkoesp8@gmail.com>
2023-03-17 14:11:30 +0100

    aggiunge funzione stampaArray completata

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+printArray(new[] { 2, 6, 7, 5, 3, 9 });
+
+void printArray(int[] array)
+{
+    Console.WriteLine(arrayToString(array));
+}
+//Funzione che mi stampa un array di numeri interi
+string arrayToString(int[] array)
+{
+    var result = "[";
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        result += array[i].ToString();
+
+        if (i < array.Length - 1)
+        {
+            result += ", ";
+        }
+    }
+
+    result += "]";
+
+    return result;
+}
